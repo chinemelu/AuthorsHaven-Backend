f3deb59bdea9e7cb498f592704a2aec6e271f0b5
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2019-11-28 23:42:06 +0100

    fix failing tests

--- server/models/index.js
+++ server/models/index.js
@@ -3,13 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 const client = () => mongoose
   .connect(process.env.DATABASE_URL,
     {
       useNewUrlParser: true,
       useFindAndModify: false,
-      replicaSet: 'rs',
+      replicaSet: process.env.NODE_ENV === 'development' ? 'rs' : '',
       useUnifiedTopology: true
     });
 
